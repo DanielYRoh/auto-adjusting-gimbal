a550c39ac86df6244507198830fd4e25e6055d39
DanielYRoh <danielroh05@gmail.com>
2026-03-21 23:14:13 -0700

    Fully implemented Auto-Adjusting Gimbal logic using the Servo library, MPU-6050 accelerometer data, and the atan2 function.

--- Auto_Adjusting_Gimbal.ino
+++ Auto_Adjusting_Gimbal.ino
@@ -1,18 +1,27 @@
 #include "Wire.h"
+#include "Servo.h"
 
 const int address = 0x68;
+const int servoPin = 9;
 
 int16_t accelerometer_x;
 int16_t accelerometer_y;
+int16_t accelerometer_z;
 
 const int ACCEL_XOUT_H = 0x3B;
 const int PWR_MGMT_1 = 0x6B;
+
+int angle=90;
+int offset=0;
+
+Servo myServo;
 
 
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  myServo.attach(servoPin);
 
   Wire.begin(); //Initiates the wire library.
 
@@ -25,21 +34,41 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  myServo.write(angle);
   Wire.beginTransmission(address);
   Wire.write(ACCEL_XOUT_H); //Starting with register 0x3B.
   Wire.endTransmission(false); //sends a restart condition
-  Wire.requestFrom(address, 4, true); //Registers 0x3B-0x3E - Accelerometer Measurements (x,y)
+  Wire.requestFrom(address, 6, true); //Registers 0x3B-0x40 - Accelerometer Measurements (x,y,z)
   
   accelerometer_x = (Wire.read()<<8) | Wire.read(); //Shift upper half 8 bits left, then use OR operator with lower half to combine them.
   accelerometer_y =  (Wire.read()<<8) | Wire.read();
+  accelerometer_z =  (Wire.read()<<8) | Wire.read();
 
   Serial.print("x = ");
-  Serial.print(accelerometer_x);
+  Serial.print(accelerometer_x); //printing the raw values
 
   Serial.print("|  y = ");
-  Serial.println(accelerometer_y);
+  Serial.print(accelerometer_y);
 
-  delay(1000);
+  Serial.print("|  z = ");
+  Serial.print(accelerometer_z);
+
+
+  offset = atan2(accelerometer_y, accelerometer_z)*(180)/PI; //atan2 returns angle in radians. Takes care of the sign and divide by zero problem.
+  
+  angle = 90+offset;
+  angle = constrain(angle, 0,180); //preventing offset from going beyond 180/
+
+  
+  myServo.write(angle);
+
+  Serial.print("| angle = ");
+  Serial.print(angle);
+
+  Serial.print("| offset = ");
+  Serial.println(offset);
+
+  delay(100);
 
 
 }
